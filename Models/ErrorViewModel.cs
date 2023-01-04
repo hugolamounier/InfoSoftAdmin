6c3d5bcb55f5efc069c566d7572e6d0003581370
Hugo Lamounier <hugolamo@gmail.com>
2023-01-03 22:16:59 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InfoSoftAdmin.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
